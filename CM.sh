8de43cc632b8fcd78c6359f179739fff3b46bb6d
pbhanda2 <pbhanda2@ncsu.edu>
2018-02-13 12:29:22 -0500

    Add files via upload

---
+++ CM.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+npm install
+# export DOTOKEN = "DIGITAL-OCEAN-TOKEN-HERE"
+node DigitalOcean.js
+# export AWS_ACCESS_KEY_ID = "AWS-ACCESS-KEY-ID-HERE"
+# export AWS_SECRET_ACCESS_KEY = "AWS-SECRET-ACCESS-KEY-HERE"
+node aws.jss
