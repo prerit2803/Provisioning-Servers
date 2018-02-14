13167436e8fbd49b0d46c294386e767d774d2adf
pbhanda2 <pbhanda2@ncsu.edu>
2018-02-13 20:21:54 -0500

    Update CM.sh

--- CM.sh
+++ CM.sh
@@ -3,14 +3,14 @@ echo "----------------------------------------------------"
 echo "Welcome to Provisioning Server Demo"
 echo "----------------------------------------------------"
 npm install
-export DOTOKEN = "DIGITAL-OCEAN-TOKEN-HERE"
 echo "----------------------------------------------------"
 echo "Creating Digital Ocean Instance"
 echo "----------------------------------------------------"
+export DOTOKEN = "DIGITAL-OCEAN-TOKEN-HERE"
 node DigitalOcean.js
-export AWS_ACCESS_KEY_ID = "AWS-ACCESS-KEY-ID-HERE"
-export AWS_SECRET_ACCESS_KEY = "AWS-SECRET-ACCESS-KEY-HERE"
 echo "----------------------------------------------------"
 echo "Creating AWS EC2 Instance"
 echo "----------------------------------------------------"
+export AWS_ACCESS_KEY_ID = "AWS-ACCESS-KEY-ID-HERE"
+export AWS_SECRET_ACCESS_KEY = "AWS-SECRET-ACCESS-KEY-HERE"
 node AWS.js
